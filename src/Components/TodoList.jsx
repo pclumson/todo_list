a34707b219eb3f1b86e78f423a43666a0b89c6bd
Prince Eklu <ameto888@protonmail.com>
2024-09-20 00:53:16 -0400

    Setting the initial State.

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+
   
 
   return (
